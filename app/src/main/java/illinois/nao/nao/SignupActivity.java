e080b0cbede3f2ab85cacbf737907672f44be5d0
Eric <orangepamelo@gmail.com>
2016-10-22 20:27:28 -0500

    Delete unused imports

--- app/src/main/java/illinois/nao/nao/SignupActivity.java
+++ app/src/main/java/illinois/nao/nao/SignupActivity.java
@@ -1,15 +1,11 @@
 package illinois.nao.nao;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttributes;
