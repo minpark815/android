0bae995e569d7c4696bb7f5cdbe6e280a268e1e6
Eric <orangepamelo@gmail.com>
2016-10-25 17:25:37 -0500

    Fix Play Audio button

--- app/src/main/java/illinois/nao/nao/Pages/ProfileFragment.java
+++ app/src/main/java/illinois/nao/nao/Pages/ProfileFragment.java
@@ -4,10 +4,12 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ScrollView;
 
 import butterknife.BindView;
@@ -17,10 +19,11 @@ import nz.co.delacour.exposurevideoplayer.ExposureVideoPlayer;
 
 
 public class ProfileFragment extends Fragment {
-
     @BindView(R.id.profile_videoplayer) ExposureVideoPlayer videoPlayer;
-    @BindView(R.id.profile_button_audio) Button buttonAudio;
+    @BindView(R.id.profile_button_audio) ImageButton buttonAudio;
     @BindView(R.id.scrollView_profile) ScrollView scrollView;
+
+    private MediaPlayer mp;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,14 +38,27 @@ public class ProfileFragment extends Fragment {
         ButterKnife.bind(this, view);
 
         videoPlayer.setVideoSource(Uri.parse("android.resource://illinois.nao.nao/" + R.raw.naovideo));
+        mp = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.ifelephantscouldfly);
 
         buttonAudio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final MediaPlayer mp = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.ifelephantscouldfly);
-                mp.start();
+                Log.i("Media Player", "Is Playing: " + mp.isPlaying());
+
+                if(mp.isPlaying()) {
+                    mp.pause();
+                    //mp.stop();
+                    Log.i("Media Player", "Pause");
+                    buttonAudio.setImageResource(R.drawable.ic_play_arrow_black_24dp);
+                } else {
+                    mp.start();
+                    Log.i("Media Player", "Play");
+                    buttonAudio.setImageResource(R.drawable.ic_pause_black_24dp);
+                }
             }
         });
+
+
         return view;
     }
 }
