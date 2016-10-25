0a44a8e193d770d572b51f56b580b05eb998e047
Eric <orangepamelo@gmail.com>
2016-10-25 05:28:20 -0500

    Import Video Library for heavy lifting

--- app/src/main/java/illinois/nao/nao/Pages/ProfileFragment.java
+++ app/src/main/java/illinois/nao/nao/Pages/ProfileFragment.java
@@ -2,24 +2,24 @@ package illinois.nao.nao.Pages;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ScrollView;
-import android.widget.VideoView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import illinois.nao.nao.R;
+import nz.co.delacour.exposurevideoplayer.ExposureVideoPlayer;
 
 
 public class ProfileFragment extends Fragment {
 
-    @BindView(R.id.videoView) VideoView videoView;
-    @BindView(R.id.button_audio) Button buttonAudio;
+    @BindView(R.id.profile_videoplayer) ExposureVideoPlayer videoPlayer;
+    @BindView(R.id.profile_button_audio) Button buttonAudio;
+    @BindView(R.id.scrollView_profile) ScrollView scrollView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,18 +33,7 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         ButterKnife.bind(this, view);
 
-        videoView.setVideoURI(Uri.parse("android.resource://illinois.nao.nao/" + R.raw.naovideo));
-
-        buttonAudio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(videoView.isPlaying()) {
-                    videoView.pause();
-                } else {
-                    videoView.start();
-                }
-            }
-        });
+        videoPlayer.setVideoSource(Uri.parse("android.resource://illinois.nao.nao/" + R.raw.naovideo));
 
         return view;
     }
